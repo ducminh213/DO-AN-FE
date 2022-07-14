48712696c34b28279f7662caa26be541197acb27
ducminh213 <92130001+ducminh213@users.noreply.github.com>
2022-07-14 13:52:47 +0700

    Add files via upload

---
+++ public/js/app.js
@@ -0,0 +1,18 @@
+
+$('.owl-carousel').owlCarousel({
+  stagePadding: 50,
+  loop:true,
+  margin:30,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+      1000:{
+          items:3
+      }
+  }
+})
